51d3e2c0f1e06c9cb3180b2d58685cdb61cd1ec8
IDStack <ahmadhakim404@gmail.com>
2021-06-23 21:19:01 +0700

    make model user and role with config

--- src/server.js
+++ src/server.js
@@ -6,6 +6,34 @@ const app = express()
 
 dotenv.config()
 
+const db = require('./models')
+const Role = db.role
+
+db.sequelize.sync({ force: true }).then(() => {
+  console.log('Drop and Resync Db')
+  initial()
+})
+
+// use this for avoid drop table
+//db.sequelize.sync();
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: 'user',
+  })
+
+  Role.create({
+    id: 2,
+    name: 'moderator',
+  })
+
+  Role.create({
+    id: 3,
+    name: 'admin',
+  })
+}
+
 let corsOptions = {
   origin: 'http://localhost:8080',
 }
